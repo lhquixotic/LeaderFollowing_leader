50757ea3ecf9ab0686122e64bbe029ce7686b711
CamelFucker <69066073+CamelFucker@users.noreply.github.com>
2020-08-30 12:31:47 +0800

    add UWB A8

--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@ int main()
     //follower_com.CAN_retrans(VEHICLE_BRAKE_MSG);
     static int CANmsg_origin_brake[9] = {0};
     static int CANmsg_origin_pedal[9] = {0};
+    static int CANmsg_leader_uwb[9] = {0};
     thread th1(&Communication::CAN_retrans,&follower_com,VEHICLE_BRAKE_MSG,CANmsg_origin_brake);
     thread th2(&Communication::CAN_retrans,&follower_com,VEHICLE_PEDAL_ANGLE_MSG,CANmsg_origin_pedal);
+    thread th3(&Communication::CAN_retrans,&follower_com,LEADER_UWB_MSG,CANmsg_leader_uwb);
 
     th1.join();
     th2.join();
-
-
+    th3.join();
 
     return 0;
 }
